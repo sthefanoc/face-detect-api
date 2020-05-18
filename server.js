56ce41d42b85991c8bd1582693f910a2d1e1ba69
Sthefano Carvalho <sthefanocarvalho.consultoria@gmail.com>
2020-05-18 18:10:22 -0300

    Working api. Node modules deleted

--- server.js
+++ server.js
@@ -1,39 +1,111 @@
 const express = require('express');
-
 const app = express();
+const bodyParser = require('body-parser');
+const bcrypt= require('bcrypt-nodejs');
+let cors = require('cors');
+
+
+const database ={
+    users: [
+        {
+            id: '123',
+            name: 'Sthefano',
+            email: 'sthefano@masteradin.com',
+            password: 'bolacha',
+            entries: 0,
+            joined: new Date()
+        },
+        {
+            id: '124',
+            name: 'Aynoan',
+            email: 'aynoan@fofinha.com',
+            password: 'biscoito',
+            entries: 0,
+            joined: new Date()
+        },
+        
+        
+    ],
+    secrets: {
+    users_id: '123',
+    hash: 'wghhh'
+  }
+}
+
+app.use(cors());
+app.use(bodyParser.json());
 
 app.get('/',(req,res) => {
-    res.send('this is working')
+    res.send(database.users)
 })
 
 app.post('/signin', (req, res) => {
-    res.send('signin is working')
-}
+    let info = req.body;
+    if (info.email === database.users[0].email &&
+            info.password === database.users[0].password) {
+        res.json(database.users[0])
+    } else {
+        res.status(400).json("error logging in")
+    }
+})
 
 app.post('/register', (req, res) => {
-    res.send('register is working')
-}
+    const { email, name, password } = req.body;
+    bcrypt.hash(password, null, null, function(err, hash) {
+        console.log(hash);
+      });
+    database.users.push({
+        id: (Number(database.users[database.users.length - 1].id) + 1).toString(),
+        name: name,
+        email: email,
+        entries: 0,
+        joined: new Date()
+    });
+    res.json(database.users[database.users.length -1]);
+})
 
-app.get('/profile/:userId', (req, res) => {
-    res.send('profile is working')
+app.get('/profile/:id', (req, res) => {
+    const { id } = req.params;
+    let found = false;
+    database.users.forEach(user => {
+        if (user.id === id) {
+            found = true;
+            return res.json(user);
+        }
+    })
+    if (!found) {
+        res.status(400).json("not found");
+    }
 })
 
 app.put('/image', (req, res)=> {
-    res.send('image is working')
-})
+    const { id } = req.body;
+    let found = false;
+    database.users.forEach(user => {
+        if (user.id === id) {
+            found = true;
+            user.entries++
+            return res.json(user.entries);
+        }
+    })
+    if (!found) {
+        res.status(400).json("not found");
+    }
+});
 
-app.listen(3000, ()=> {
+// bcrypt.hash("bacon", null, null, function(err, hash) {
+//     // Store hash in your password DB.
+//   });
+  
+//   // Load hash from your password DB.
+//   bcrypt.compare("bacon", hash, function(err, res) {
+//       // res == true
+//   });
+//   bcrypt.compare("veggies", hash, function(err, res) {
+//       // res = false
+//   });
+
+
+  app.listen(3000, ()=> {
     console.log('App is running on: http://localhost:3000/');
 })
-
-/*
-Routes:
-/ --> res = this is working
-/signin --> POST = success/ fail
-/register --> POST = user object
-
-/profile/:userId --> GET = user
-/image --> PUT --> user count
-
-*/
-
